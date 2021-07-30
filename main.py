9798fe056a0eeef9a1ee7cbb812f57003f83735b
PCW-OFICIAL <84603577+PCW-OFICIAL@users.noreply.github.com>
2021-07-30 11:00:01 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import os, sys
+def restart():
+	python=sys.executable;os.excl(python, python, *sys.argv)
+try:
+	import colorama, requests
+except:
+	os.system('pip install colorama requests')
+try:
+	from data import ui, numero, cpf, nome, rg, email
+except Exception as e:
+	print('ARQUIVO CORROMPIDO! '+str(e));exit()
+C= "\033[97;1m"
+G = "\033[92;1m"
+P = "\033[1;35m"
+Sair=False
+while(Sair==False):
+	try:
+		op=int(ui.menu(ms0=f'\n{C}[{G}1{C}] Numero\n{C}[{G}2{C}] CPF\n{C}[{G}3{C}] Nome\n{C}[{G}4{C}] Email\n{C}[{G}5{C}] RG\n{C}[{P}0{C}] Sair'))
+		if op==1:
+			numero.consultar()
+		elif op==2:
+			cpf.consultar()
+		elif op==3:
+			nome.consultar()
+		elif op==4:
+			email.consultar()
+		elif op==5:
+			rg.consultar()
+		elif op==0:
+			ui.clear();Sair=True
+	except:
+		ui.error()
