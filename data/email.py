268df394b2dd597d76ad319b79172cb484266bbe
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-07-30 17:17:32 -0300

    Update email.py

--- data/email.py
+++ data/email.py
@@ -6,7 +6,7 @@ def consultar():
 		email = ui.dialog1()
 		r=requests.get(f'https://dualityapi.xyz/apis/teste/Consultas%20Privadas/HTML/email.php?consulta={email}').text
 		if 'A consulta Esta Funcionando Normalmente' in r:
-			msg='A consulta está funcionando normalmente, porém, o Telefone inserido não foi encontrado.'
+			msg='A consulta está funcionando normalmente, porém, o Email inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n').replace('CPF', '\nCPF')
 		op=int(ui.dialog2(msg))
