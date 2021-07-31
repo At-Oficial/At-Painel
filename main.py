8853cb7eb64caf93c3430c957c54ec24039dcce2
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-07-30 23:10:24 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ try:
 except:
 	os.system('pip install colorama requests')
 try:
-	from data import ui, numero, cpf, nome, rg, email
+	from data import ui, numero, cpf, nome, rg, email,nome2,datan, cep, placa
 except Exception as e:
 	print('ARQUIVO CORROMPIDO! '+str(e));exit()
 C= "\033[97;1m"
@@ -16,17 +16,29 @@ P = "\033[1;35m"
 Sair=False
 while(Sair==False):
 	try:
-		op=int(ui.menu(ms0=f'\n{C}[{G}1{C}] Numero\n{C}[{G}2{C}] CPF\n{C}[{G}3{C}] Nome\n{C}[{G}4{C}] Email\n{C}[{G}5{C}] RG\n{C}[{P}0{C}] Sair'))
+		op=int(ui.menu(ms0=f'\n{C}[{G}1{C}] Numero\n{C}[{G}2{C}] CPF\n{C}[{G}3{C}] Nome\n{C}[{G}4{C}] Email\n{C}[{G}5{C}] RG\n{C}[{G}6{C}] Data de Nascimento\n{C}[{G}7{C}] CEP\n{C}[{G}8{C}] Placa\n\n{C}[{P}0{C}] Sair'))
 		if op==1:
 			numero.consultar()
 		elif op==2:
 			cpf.consultar()
 		elif op==3:
-			nome.consultar()
+			choice = int(ui.menu(ms0=f'\n{C}[{G}1{C}] Nome-1\n{C}[{G}2{C}] Nome-2'))
+			if choice ==1:
+				nome.consultar()
+			elif choice ==2:
+				nome2.consultar()
+			else:
+				ui.error()
 		elif op==4:
 			email.consultar()
 		elif op==5:
 			rg.consultar()
+		elif op==6:
+			datan.consultar()
+		elif op==7:
+			cep.consultar()
+		elif op==8:
+			placa.consultar()
 		elif op==0:
 			ui.clear();Sair=True
 	except:
