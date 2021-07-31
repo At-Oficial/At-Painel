81c5ec0b8126b369cf872e955376061d20f2703c
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-07-30 23:15:27 -0300

    Create placa.py

---
+++ data/placa.py
@@ -0,0 +1,18 @@
+import requests
+from data import ui
+def consultar():
+	Sair=False
+	while (Sair==False):
+		placa = ui.dialog1()
+		r =requests.get(f'https://dualityapi.xyz/apis/teste/Consultas%20Privadas/HTML/placa.php?consulta={placa}').text
+		if 'A consulta Esta Funcionando Normalmente' in r:
+			msg='A consulta está funcionando normalmente, porém, a placa inserida não foi encontrado.'
+		else:
+			msg=r.replace('<p>', '').replace('<br>', '\n')
+		op=int(ui.dialog2(msg))
+		if op ==1:
+			pass
+		elif op==2:
+			Sair=True
+		else:
+			ui.error()
