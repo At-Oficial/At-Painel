709fc9b51bd050e71d2c1c1783cb45c672dcda94
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-08-13 02:39:09 -0300

    Update rg.py

--- data/rg.py
+++ data/rg.py
@@ -5,6 +5,7 @@ def consultar():
 	while (Sair==False):
 		rg = ui.dialog1()
 		r = requests.get(f'https://dualityapi.xyz/apis/flex_7/Consultas%20Privadas/HTML/rg.php?consulta={rg}')
+		if 'A Consulta' in r:
 			msg='A consulta está funcionando normalmente, porém, o RG inserido não foi encontrado.'
 		else:
 			msg=r.replace('<p>', '').replace('<br>', '\n').replace('CPF', '\nCPF')
