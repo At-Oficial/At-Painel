3839b862939e44c326a3992a17cb8ab9ea72f62d
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-08-13 02:05:41 -0300

    Update placa.py

--- data/placa.py
+++ data/placa.py
@@ -4,7 +4,7 @@ def consultar():
 	Sair=False
 	while (Sair==False):
 		placa = ui.dialog1()
-		r =requests.get(f'https://dualityapi.xyz/apis/teste/Consultas%20Privadas/HTML/placa.php?consulta={placa}').text
+		r =requests.get(f'https://dualityapi.xyz/apis/flex_7/Consultas%20Privadas/HTML/placa.php?consulta={placa}').text
 		if 'A consulta Esta Funcionando Normalmente' in r:
 			msg='A consulta está funcionando normalmente, porém, a placa inserida não foi encontrado.'
 		else:
