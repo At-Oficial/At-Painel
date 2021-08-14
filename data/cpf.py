8cdd96c2ab59e1c080dadb17c3f6c4f63fcad794
At-Oficial <84603577+At-Oficial@users.noreply.github.com>
2021-08-14 14:29:36 -0300

    Update cpf.py

--- data/cpf.py
+++ data/cpf.py
@@ -3,12 +3,12 @@ from data import ui
 def consultar():
 	Sair=False
 	while (Sair==False):
-		cpf = ui.dialog1()
-		r=requests.get(f'https://dualityapi.xyz/apis/flex_7/Consultas%20Privadas/HTML/cpf.php?consulta={cpf}').text
-		if 'A Consulta' in r:
-			msg='A consulta está funcionando normalmente, porém o CPF não foi encontrado.'
+		cpf= int(ui.dialog1())
+		r = requests.get(f'https://dualityapi.xyz/apis/flex_7/Consultas%20Privadas/HTML/cpf.php?consulta={cpf}').text
+		if 'A consulta Esta Funcionando Normalmente' in r:
+			msg='A consulta está funcionando normalmente, porém, o CPF inserido não foi encontrado.'
 		else:
-			msg=r.replace('<br>, '\n').replace('<p>','')
+			msg=r.replace('<p>', '').replace('<br>', '\n').replace('DDD:', 'DDD: ').replace('NOME:', 'Nome completo: ').replace('FONE:', 'Telefone: ').replace('INST:', 'Data de Compra: ').replace('PESSOA:', 'Pessoa: ').replace('CPF:', 'CPF: ').replace('PESSOA:', 'Pessoa: ')
 		op=int(ui.dialog2(msg))
 		if op ==1:
 			pass
